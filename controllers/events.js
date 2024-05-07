e1c86315e72d0ab4fbc7fe8c5325e6900a102944
NickEsColR <nicoesol0818@gmail.com>
2024-05-07 17:12:53 -0500

    feat: add update event in BD

--- controllers/events.js
+++ controllers/events.js
@@ -26,11 +26,44 @@ const addEvent = async (req, res) => {
         });
     }
 };
-const updateEvent = (req, res) => {
-    res.json({
-        ok: true,
-        message: "PUT Event",
-    });
+const updateEvent = async(req, res) => {
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try{
+        const event = await Event.findById(eventId);
+        if(!event){
+            return res.status(404).json({
+                ok: false,
+                message: "Event not found",
+            });
+        }
+
+        if(event.user.toString() !== uid){
+            return res.status(401).json({
+                ok: false,
+                message: "You do not have the necessary permissions",
+            });
+        }
+
+        const newEvent = {
+            ...req.body,
+            user: uid,
+        };
+
+        const updatedEvent = await Event.findByIdAndUpdate(eventId, newEvent, {new: true});
+
+        res.json({
+            ok: true,
+            event: updatedEvent,
+        });
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            message: "Please contact the administrator",
+        });
+    }
 };
 const deleteEvent = (req, res) => {
     res.json({
