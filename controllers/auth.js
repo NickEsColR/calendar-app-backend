1c9cf8bc865a1e6a34fa0fa5979aed07e38f4f88
NickEsColR <nicoesol0818@gmail.com>
2024-05-07 09:53:17 -0500

    feat: add create user to db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,24 @@
-const express = require("express");
+const {response} = require("express");
+const User = require("../models/User");
 
-const createUser = (req, res) => {
-    const { name, email, password } = req.body;
+const createUser = async(req, res) => {
+    // const { name, email, password } = req.body;
 
-    res.status(201).json({
-        ok: true,
-        msg: "registro",
-        name,
-        email,
-        password,
-    });
+    try{
+        const user = new User(req.body);
+        await user.save();
+    
+        res.status(201).json({
+            ok: true,
+            msg: "registro",
+        });
+    }catch (error){
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: "Por favor hable con el administrador",
+        });
+    }
 };
 
 const loginUser = (req, res) => {
