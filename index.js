0e1c4359732a4e83c25b73d5c571570eb192895e
NickEsColR <nicoesol0818@gmail.com>
2024-05-07 09:28:16 -0500

    feat: add conection with bd

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 
+const { dbConnection } = require('./database/config');
+dbConnection();
+
 // crear el servidor
 const app = express();
 
